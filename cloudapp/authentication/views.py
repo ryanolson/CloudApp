178e1374dc719240666b259b12168c5e05a334e1
Ryan Olson <rmolson@gmail.com>
2013-04-17 22:52:27 -0600

    added copyright and copy permission statements

--- cloudapp/authentication/views.py
+++ cloudapp/authentication/views.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+Copyright 2013 Ryan Olson
+
+This file is part of CloudApp.
+
+CloudApp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CloudApp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CloudApp.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from urlparse import urlparse, urljoin
 from flask import Blueprint, request, redirect, render_template, g, url_for, current_app
 from flask.ext.wtf import Form, HiddenField
